630a5adcf9ba4f3f1f7cc42e7cdc5aa9e16b3266
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-15 15:23:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1191,7 +1191,7 @@ function showAbout() {
     <div style="padding:15px; line-height:1.6;">
       <h3>Alatiphy - AlatiphA Music</h3>
       <p>With AlatiphA, it’s easy to listen to tailored online and offline music on your phone, your computer, your tablet and more.</p>
-      <p>Version: v2.1</p>
+      <p>Version: v2.3</p>
     </div>
   `;
 }
