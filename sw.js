a4f7af861be465b0334f23f54ef29ebb8ec798f6
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-07-30 06:37:27 +0000

    Update app version from v7.2 to v7.3

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION = "v7.2";
+const APP_VERSION = "v7.3";
 
 const CACHE_NAME = `alatiphy-beta-pwa-${APP_VERSION}`;
 const SONG_CACHE = `alatiphy-songs-pwa-${APP_VERSION}`;
