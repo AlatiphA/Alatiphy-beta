0ea9bd5e4ec0528b5e1b1ada1176ca66c340f9a3
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-08-02 06:08:34 +0000

    Update app version to v7.7 in service worker

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION = "v7.6";
+const APP_VERSION = "v7.7";
 
 const CACHE_NAME = `alatiphy-beta-pwa-${APP_VERSION}`;
 const SONG_CACHE = `alatiphy-songs-pwa-${APP_VERSION}`;
@@ -22,6 +22,7 @@ const ASSETS = [
   "./index.html",
   "./faq.html",
   "./app.js",
+  "./sw.js",
   "./install.js",
   "./style.css",
   "./manifest.json",
